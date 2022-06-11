51352468fe9d81ad92cf97861e168bdddb55de85
Pablo Diaz <pablo.diazv@pucp.edu.pe>
2022-06-10 22:52:01 -0500

    do one query

--- internal/scraper/scraper.go
+++ internal/scraper/scraper.go
@@ -1,7 +1,6 @@
 package scraper
 
 import (
-	"log"
 	"net/http"
 	"strings"
 
@@ -13,15 +12,13 @@ func ParseFormatWithUsername(username string, site models.Sites) string {
 	return strings.Replace(format, "{}", username, -1)
 }
 
-func GetResponse(client *http.Client, errorType string, site models.Sites) (err error) {
-	resp, err := client.Get(site.URLFormat)
+func GetResponse(client *http.Client, url string) (err error) {
+	resp, err := client.Get(url)
 	if err != nil {
-		log.Println("[GetResponse][Error]", err)
 		return err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
-		log.Println("[GetResponse][Error]", errorType, ":", resp.StatusCode)
 		return err
 	}
 	return nil
---
+++ internal/scraper/search.go
@@ -0,0 +1,36 @@
+package scraper
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/jinzhu/gorm"
+	"github.com/pablodz/sherlockgo/internal/models"
+)
+
+func ScrapeThisUsername(db *gorm.DB, username string) {
+
+	// get all sites
+	var listSites []models.Sites
+	db.Find(listSites, &models.Sites{})
+	// create http client
+	client := &http.Client{}
+
+	// send request for all
+loopQuery:
+	for index, site := range listSites {
+		go doSearchOneSite(username, site, client)
+		if index == 10 {
+			break loopQuery
+		}
+	}
+
+}
+
+func doSearchOneSite(username string, site models.Sites, client *http.Client) {
+	url := ParseFormatWithUsername(username, site)
+	err := GetResponse(client, url)
+	if err != nil {
+		log.Println("[GetResponse][Error]", err)
+	}
+}
