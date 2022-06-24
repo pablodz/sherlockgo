5d979437e452be23f2f891d5a55293b43779c2bc
Pablo Diaz <pablo.diazv@pucp.edu.pe>
2022-06-23 22:53:20 -0500

    filtered by val

--- internal/endpoints/username/username.go
+++ internal/endpoints/username/username.go
@@ -50,3 +50,53 @@ func GETByUsername(db *gorm.DB) echo.HandlerFunc {
 		return nil
 	}
 }
+
+func GETByUsernameAndSiteFilteredByFound(db *gorm.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+		username := c.Param("username")
+		wasFound := c.Param("found")
+		var valWasFound bool
+		if wasFound == "true" || wasFound == "1" {
+			valWasFound = true
+		} else {
+			valWasFound = false
+		}
+
+		// get all sites
+		var listSites []models.Sites
+		db.Find(&listSites)
+		// create http client
+		client := &http.Client{}
+		// chain responses
+		chainResponses := make(chan models.UsernameResponse)
+
+		for _, site := range listSites {
+			go scraper.DoSearchOneSiteChain(username, site, client, chainResponses)
+			// if index == 10 {
+			// 	break
+			// }
+		}
+
+		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		c.Response().WriteHeader(http.StatusOK)
+
+		enc := json.NewEncoder(c.Response())
+		counter := 0
+		for l := range chainResponses {
+			l.IDDownload = strconv.Itoa(counter+1) + "/" + strconv.Itoa(len(listSites))
+			if l.Found == valWasFound {
+				if err := enc.Encode(l); err != nil {
+					return err
+				}
+				c.Response().Flush()
+			}
+			// time.Sleep(100 * time.Millisecond)
+			counter++
+			if counter == len(listSites) {
+				break
+			}
+		}
+		return nil
+	}
+}
