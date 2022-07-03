83ced5b0b76a9020101c5a49a87b671ed46e755c
Pablo Diaz <pablo.diazv@pucp.edu.pe>
2022-07-03 16:24:23 -0400

    remove blank spaces

--- internal/database/database.go
+++ internal/database/database.go
@@ -12,19 +12,14 @@ import (
 var DB *gorm.DB = nil
 
 func GetDB() (*gorm.DB, error) {
-
 	var err error = nil
-
 	if DB == nil {
-
 		dbName := "./" + utils.MainDbName + ".db"
 		log.Println("Reading database")
 		DB, err = gorm.Open(sqlite.Open(dbName), &gorm.Config{})
 		if err != nil {
 			log.Println("[DATABASE][Error]", err)
 		}
-
 	}
 	return DB, err
-	// defer Db.Close()
 }
